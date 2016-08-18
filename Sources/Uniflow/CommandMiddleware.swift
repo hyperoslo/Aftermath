e6a5c58a1ecbb7d93bebabac023492b573dc3931
Vadym Markov <markov.vadym@gmail.com>
2016-08-18 02:14:13 +0200

    Implement command middleware

--- Sources/Uniflow/CommandMiddleware.swift
+++ Sources/Uniflow/CommandMiddleware.swift
@@ -1,9 +1,31 @@
-//
-//  CommandMiddleware.swift
-//  Uniflow
-//
-//  Created by Vadym Markov on 18/08/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
+public typealias Execute = (Command) throws -> Void
 
-import Foundation
+public typealias MiddlewareCombination = (Execute) throws -> Execute
+
+public protocol AnyCommandMiddleware {
+  func execute(context: Any) throws -> MiddlewareCombination
+}
+
+public protocol CommandMiddleware: AnyCommandMiddleware {
+  associatedtype T: Command
+
+  func intercept(command: AnyCommand, execute: Execute, next: Execute) throws
+  func compose(execute: Execute) throws -> MiddlewareCombination
+}
+
+public extension CommandMiddleware {
+
+  func compose(execute: Execute) throws -> MiddlewareCombination {
+    return try respond { next, command in
+      return try self.intercept(command, execute: execute, next: next)
+    }
+  }
+
+  func respond(handle: (Execute, AnyCommand) throws -> Void) throws -> MiddlewareCombination {
+    return { next in
+      return { command in
+        try handle(next, command)
+      }
+    }
+  }
+}
