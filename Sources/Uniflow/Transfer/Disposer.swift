7ef3b584300ae48a6ed5a2c66916c2720324d28d
Vadym Markov <markov.vadym@gmail.com>
2016-08-19 16:02:29 +0200

    Disposer protocol

--- Sources/Uniflow/Transfer/Disposer.swift
+++ Sources/Uniflow/Transfer/Disposer.swift
@@ -1,11 +1,32 @@
 import Foundation
 
-//@noescape
-
+// MARK: - Disposal
 
 public typealias DisposalToken = String
 
 public protocol Disposer {
   func dispose(token: DisposalToken)
   func disposeAll()
+}
+
+// MARK: - Mutex disposer
+
+protocol MutexDisposer: class, Disposer {
+  var mutex: pthread_mutex_t { get set }
+  var listeners: [DisposalToken: Listener] { get set }
+}
+
+extension MutexDisposer {
+
+  func dispose(token: String) {
+    pthread_mutex_lock(&mutex)
+    listeners.removeValueForKey(token)
+    pthread_mutex_unlock(&mutex)
+  }
+
+  func disposeAll() {
+    pthread_mutex_lock(&mutex)
+    listeners.removeAll()
+    pthread_mutex_unlock(&mutex)
+  }
 }
