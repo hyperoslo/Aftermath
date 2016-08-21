178437e7d594f75584ea17196aecc8c517599284
Vadym Markov <markov.vadym@gmail.com>
2016-08-21 19:34:33 +0200

    Add more test mocks

--- Tests/Uniflow/Helpers/Mocks.swift
+++ Tests/Uniflow/Helpers/Mocks.swift
@@ -63,8 +63,42 @@ struct LogEventMiddleware: EventMiddleware {
 
   var callback: (AnyEvent -> Void)?
 
-  func intercept(command: AnyEvent, execute: Publish, next: Publish) throws {
-    callback?(command)
-    try next(command)
+  func intercept(event: AnyEvent, publish: Publish, next: Publish) throws {
+    callback?(event)
+    try next(event)
   }
+}
+
+struct AbortEventMiddleware: EventMiddleware {
+
+  var callback: (AnyEvent -> Void)?
+
+  func intercept(command: AnyEvent, publish: Publish, next: Publish) throws {
+    callback?(command)
+  }
+}
+
+struct ErrorEventMiddleware: EventMiddleware {
+
+  var callback: (AnyEvent -> Void)?
+
+  func intercept(event: AnyEvent, publish: Publish, next: Publish) throws {
+    callback?(event)
+
+    guard let calcEvent = event as? Event<Calculator> else {
+      try next(event)
+      return
+    }
+
+    switch calcEvent {
+    case .Success:
+      try publish(Event<Calculator>.Error(TestError.Test))
+    default:
+      try next(event)
+    }
+  }
+}
+
+enum TestError: ErrorType {
+  case Test
 }
