cde9c6f883f15a4a6da02faed9b20ce8e3aad2bc
Vadym Markov <markov.vadym@gmail.com>
2016-08-21 18:18:06 +0200

    Add test mocks

--- Tests/Uniflow/Helpers/Mocks.swift
+++ Tests/Uniflow/Helpers/Mocks.swift
@@ -1,9 +1,70 @@
-//
-//  Mocks.swift
-//  Uniflow
-//
-//  Created by Vadym Markov on 21/08/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
+@testable import Uniflow
 
-import Foundation
+// MARK: - Commands
+
+struct Calculator: Projection {
+  let result: Int
+}
+
+struct SubtractionCommand: Command {
+  typealias ProjectionType = Calculator
+
+  let value1: Int
+  let value2: Int
+}
+
+struct AdditionCommand: Command {
+  typealias ProjectionType = Calculator
+
+  let value1: Int
+  let value2: Int
+}
+
+// MARK: - Command middleware
+
+struct LogCommandMiddleware: CommandMiddleware {
+
+  var callback: (AnyCommand -> Void)?
+
+  func intercept(command: AnyCommand, execute: Execute, next: Execute) throws {
+    callback?(command)
+    try next(command)
+  }
+}
+
+struct AbortCommandMiddleware: CommandMiddleware {
+
+  var callback: (AnyCommand -> Void)?
+
+  func intercept(command: AnyCommand, execute: Execute, next: Execute) throws {
+    callback?(command)
+  }
+}
+
+struct AdditionCommandMiddleware: CommandMiddleware {
+
+  var callback: (AnyCommand -> Void)?
+
+  func intercept(command: AnyCommand, execute: Execute, next: Execute) throws {
+    callback?(command)
+
+    switch command {
+    case let command as SubtractionCommand:
+      try execute(AdditionCommand(value1: command.value1, value2: command.value2))
+    default:
+      try next(command)
+    }
+  }
+}
+
+// MARK: - Event middleware
+
+struct LogEventMiddleware: EventMiddleware {
+
+  var callback: (AnyEvent -> Void)?
+
+  func intercept(command: AnyEvent, execute: Publish, next: Publish) throws {
+    callback?(command)
+    try next(command)
+  }
+}
