f49cf111b5241b70a1e0ef391eeee0588c47e439
Vadym Markov <markov.vadym@gmail.com>
2016-08-21 21:43:00 +0200

    Reaction producer tests

---
+++ Tests/Uniflow/ReactionProducerTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import Uniflow
+
+class ReactionProducerTests: XCTestCase {
+
+  var producer: ReactionProducer!
+
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  // MARK: - Tests
+
+  func testInit() {
+    
+  }
+}
