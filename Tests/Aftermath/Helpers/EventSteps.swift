941e04cf893b181e9d72a5df0e92b1a52d02f61b
Vadym Markov <markov.vadym@gmail.com>
2016-08-23 00:03:00 +0200

    Add event steps

--- Tests/Aftermath/Helpers/EventSteps.swift
+++ Tests/Aftermath/Helpers/EventSteps.swift
@@ -1,9 +1,57 @@
-//
-//  EventSteps.swift
-//  Aftermath
-//
-//  Created by Vadym Markov on 22/08/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
+import XCTest
+@testable import Aftermath
 
-import Foundation
+// MARK: - Types
+
+enum EventStep {
+  case Middleware(AnyEvent, EventMiddleware)
+  case Reaction(Any)
+}
+
+protocol EventStepAsserting: class {
+  var eventSteps: [EventStep] { get set }
+}
+
+// MARK: - Helpers
+
+extension EventStepAsserting {
+
+  func addMiddlewareStep(middleware: EventMiddleware) -> (AnyEvent) -> Void {
+    return { event in
+      self.eventSteps.append(EventStep.Middleware(event, middleware))
+    }
+  }
+
+  func addReactionStep(reaction: Any) {
+    self.eventSteps.append(EventStep.Reaction(reaction))
+  }
+
+  func assertMiddlewareStep(index: Int, expected: (middleware: EventMiddleware, event: AnyEvent)) {
+    guard index >= 0 && index < eventSteps.count else {
+      XCTFail("Invalid step index")
+      return
+    }
+
+    switch eventSteps[index] {
+    case .Middleware(let event, let middleware):
+      XCTAssertTrue(event.dynamicType == expected.event.dynamicType)
+      XCTAssertTrue(middleware.dynamicType == expected.middleware.dynamicType)
+    default:
+      XCTFail("Not a middleware step")
+    }
+  }
+
+  func assertReactionStep(index: Int, expected: Any) {
+    guard index > 0 && index < eventSteps.count else {
+      XCTFail("Invalid step index")
+      return
+    }
+
+    switch eventSteps[index] {
+    case .Reaction(let reaction):
+      XCTAssertTrue(reaction.dynamicType == expected.dynamicType)
+    default:
+      XCTFail("Not a reaction step")
+    }
+  }
+}
