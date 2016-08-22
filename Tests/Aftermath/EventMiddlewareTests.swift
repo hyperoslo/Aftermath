2be044151601186462363dc72bf07289615e37d1
Vadym Markov <markov.vadym@gmail.com>
2016-08-23 00:55:42 +0200

    Add event middleware publish test

--- Tests/Aftermath/EventMiddlewareTests.swift
+++ Tests/Aftermath/EventMiddlewareTests.swift
@@ -27,7 +27,7 @@ class EventMiddlewareTests: XCTestCase, EventStepAsserting {
 
     listener = { event in
       self.reaction.invoke(with: event)
-      self.addReactionStep(self.reaction)
+      self.addReactionStep(event)
     }
 
     result = 0
@@ -61,6 +61,33 @@ class EventMiddlewareTests: XCTestCase, EventStepAsserting {
 
     assertMiddlewareStep(0, expected: (middleware: m1, event: event))
     assertMiddlewareStep(1, expected: (middleware: m2, event: event))
-    assertReactionStep(2, expected: reaction)
+    assertReactionStep(2, expected: event)
+  }
+
+  func testPublish() {
+    var m1 = LogEventMiddleware()
+    var m2 = ErrorEventMiddleware()
+    var m3 = LogEventMiddleware()
+    let successEvent = Event.Success(Calculator(result: 11))
+    let errorEvent = Event<Calculator>.Error(TestError.Test)
+
+    m1.callback = addMiddlewareStep(m1)
+    m2.callback = addMiddlewareStep(m2)
+    m3.callback = addMiddlewareStep(m3)
+
+    eventBus.middlewares = [m1, m2, m3]
+    eventBus.listen(listener)
+    eventBus.publish(successEvent)
+
+    XCTAssertEqual(eventSteps.count, 6)
+    XCTAssertEqual(result, 0)
+    XCTAssertTrue(error is TestError)
+
+    assertMiddlewareStep(0, expected: (middleware: m1, event: successEvent))
+    assertMiddlewareStep(1, expected: (middleware: m2, event: successEvent))
+    assertMiddlewareStep(2, expected: (middleware: m1, event: errorEvent))
+    assertMiddlewareStep(3, expected: (middleware: m2, event: errorEvent))
+    assertMiddlewareStep(4, expected: (middleware: m3, event: errorEvent))
+    assertReactionStep(5, expected: errorEvent)
   }
 }
