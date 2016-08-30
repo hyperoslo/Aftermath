977064d4a8d509077f13d7d62941440dcdf25791
Vadym Markov <markov.vadym@gmail.com>
2016-08-30 10:46:23 +0200

    Add basic models

--- Example/AftermathDemo/AftermathDemo/Sources/Models/User.swift
+++ Example/AftermathDemo/AftermathDemo/Sources/Models/User.swift
@@ -1,9 +1,22 @@
-//
-//  User.swift
-//  AftermathDemo
-//
-//  Created by Vadym Markov on 30/08/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+import Tailor
+import Sugar
+
+struct User: SafeMappable {
+
+  let id: Int
+  let name: String
+  let username: String
+  let email: String
+  let phone: String
+  let website: String
+
+  init(_ map: JSONDictionary) throws {
+    id = try <-map.property("id")
+    name = try <-map.property("name")
+    username = try <-map.property("username")
+    email = try <-map.property("email")
+    phone = try <-map.property("phone")
+    website = try <-map.property("website")
+  }
+}
