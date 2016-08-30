80fcd158420896dbaf7068915d6c90612fa83d0d
Vadym Markov <markov.vadym@gmail.com>
2016-08-30 20:00:27 +0200

    Add complete handler

--- Sources/Aftermath/Reaction.swift
+++ Sources/Aftermath/Reaction.swift
@@ -5,15 +5,18 @@ public struct Reaction<T> {
   public typealias Progress = () -> Void
   public typealias Done = (T) -> Void
   public typealias Fail = (ErrorType) -> Void
+  public typealias Complete = () -> Void
 
   public var progress: Progress?
   public var done: Done?
   public var fail: Fail?
+  public var complete: Complete?
 
-  public init(progress: Progress? = nil, done: Done? = nil, fail: Fail? = nil) {
+  public init(progress: Progress? = nil, done: Done? = nil, fail: Fail? = nil, complete: Complete? = nil) {
     self.progress = progress
     self.done = done
     self.fail = fail
+    self.complete = complete
   }
 
   func invoke<U: Command where U.Output == T>(with event: Event<U>) {
@@ -22,8 +25,10 @@ public struct Reaction<T> {
       progress?()
     case .Success(let result):
       done?(result)
+      complete?()
     case .Error(let error):
       fail?(error)
+      complete?()
     }
   }
 }
@@ -43,7 +48,12 @@ public extension ReactionProducer {
   func react<T: Command>(to command: T.Type,
              progress: Reaction<T.Output>.Progress? = nil,
              done: Reaction<T.Output>.Done,
-             fail: Reaction<T.Output>.Fail? = nil) {
-    react(to: T.self, with: Reaction<T.Output>(progress: progress, done: done, fail: fail))
+             fail: Reaction<T.Output>.Fail? = nil,
+             complete: Reaction<T.Output>.Complete? = nil) {
+    react(to: T.self, with: Reaction<T.Output>(
+      progress: progress,
+      done: done,
+      fail: fail,
+      complete: complete))
   }
 }
