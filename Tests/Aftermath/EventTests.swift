4ef471f7fa2a382502a8b09b0bb5372d02ac37a9
Vadym Markov <markov.vadym@gmail.com>
2016-08-30 19:25:16 +0200

    Test event error

--- Tests/Aftermath/EventTests.swift
+++ Tests/Aftermath/EventTests.swift
@@ -15,10 +15,21 @@ class EventTests: XCTestCase {
     XCTAssertNil(progressEvent.result)
 
     let result = "Success"
-    let successEvent = Event<TestCommand>.Success("Success")
+    let successEvent = Event<TestCommand>.Success(result)
     XCTAssertEqual(successEvent.result as? String, result)
 
     let errorEvent = Event<TestCommand>.Error(TestError.Test)
     XCTAssertNil(errorEvent.result)
+  }
+
+  func testError() {
+    let progressEvent = Event<TestCommand>.Progress
+    XCTAssertNil(progressEvent.error)
+
+    let successEvent = Event<TestCommand>.Success("Success")
+    XCTAssertNil(successEvent.error)
+
+    let errorEvent = Event<TestCommand>.Error(TestError.Test)
+    XCTAssertTrue(errorEvent.error is TestError)
   }
 }
