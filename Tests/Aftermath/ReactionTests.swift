9e37dd6e5a611fe13c18c4d4f5ac2dfce01ba03e
Vadym Markov <markov.vadym@gmail.com>
2016-08-30 20:03:49 +0200

    Test new complete handler

--- Tests/Aftermath/ReactionTests.swift
+++ Tests/Aftermath/ReactionTests.swift
@@ -5,11 +5,14 @@ class ReactionTests: XCTestCase {
 
   var reaction: Reaction<Calculator>!
   var state: State?
+  var completed = false
 
   override func setUp() {
     super.setUp()
 
     state = nil
+    completed = false
+
     reaction = Reaction(
       progress: {
         self.state = .Progress
@@ -19,7 +22,11 @@ class ReactionTests: XCTestCase {
       },
       fail: { error in
         self.state = .Error
-      })
+      },
+      complete: {
+        self.completed = true
+      }
+    )
   }
 
   override func tearDown() {
@@ -34,12 +41,14 @@ class ReactionTests: XCTestCase {
     XCTAssertNil(reaction.progress)
     XCTAssertNil(reaction.done)
     XCTAssertNil(reaction.fail)
+    XCTAssertNil(reaction.complete)
   }
 
   func testInitWithParameters() {
     XCTAssertNotNil(reaction.progress)
     XCTAssertNotNil(reaction.done)
     XCTAssertNotNil(reaction.fail)
+    XCTAssertNotNil(reaction.complete)
   }
 
   func testInvokeWithProgress() {
@@ -47,6 +56,7 @@ class ReactionTests: XCTestCase {
     reaction.invoke(with: event)
 
     XCTAssertEqual(state, .Progress)
+    XCTAssertFalse(completed)
   }
 
   func testInvokeWithSuccess() {
@@ -54,6 +64,7 @@ class ReactionTests: XCTestCase {
     reaction.invoke(with: event)
 
     XCTAssertEqual(state, .Success)
+    XCTAssertTrue(completed)
   }
 
   func testInvokeWithError() {
@@ -61,5 +72,6 @@ class ReactionTests: XCTestCase {
     reaction.invoke(with: event)
 
     XCTAssertEqual(state, .Error)
+    XCTAssertTrue(completed)
   }
 }
